99fafdc0133ef1bab70b49b508b6063f47da2fa2
Marine Sourin <marinesourin@live.fr>
2019-02-13 15:48:13 +0100

    Yelp MVC

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  '+44 20 7420 9324',
+    category: 'japanese'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number:  '+44 20 7729 1888',
+    category: 'italian'
+  },
+  {
+    name:         'Benedict',
+    address:      '19 Rue Sainte-Croix de la Bretonnerie, 75004 Paris',
+    phone_number:  '01 42 76 91 37',
+    category: 'belgian'
+  },
+  {
+    name:         'Pitaya',
+    address:      '121 Rue Oberkampf, 75011 Paris',
+    phone_number:  '01 85 15 60 05',
+    category: 'chinese'
+  },
+  {
+    name:         'La Rotonde de la Muette',
+    address:      '12 Chaussée de la Muette, 75016 Paris',
+    phone_number:  '01 45 24 45 45',
+    category: 'french'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
